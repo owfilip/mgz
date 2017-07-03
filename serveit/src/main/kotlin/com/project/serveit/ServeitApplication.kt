f426047ec01f7d1ea114413c64cc4c462ab79279
owfilip <owfilip@gmail.com>
2017-07-03 21:02:40 +0200

    Initial version of second project

---
+++ serveit/src/main/kotlin/com/project/serveit/ServeitApplication.kt
@@ -0,0 +1,11 @@
+package com.project.serveit
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ServeitApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ServeitApplication::class.java, *args)
+}
