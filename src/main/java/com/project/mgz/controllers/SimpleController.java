87117572640f46f48e5c923f376988fc03925ceb
owfilip <owfilip@gmail.com>
2017-07-03 20:50:16 +0200

    Polygon project final version

--- src/main/java/com/project/mgz/controllers/SimpleController.java
+++ src/main/java/com/project/mgz/controllers/SimpleController.java
@@ -1,5 +1,7 @@
 package com.project.mgz.controllers;
 
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +12,14 @@ public class SimpleController {
     @Value("${spring.application.name}")
     private String applicationName;
 
+    @Autowired
+    private DSLContext dsl;
+
     @RequestMapping
     public String helloPage(Model model) {
         model.addAttribute("appName", applicationName);
+
+//        dsl.insertInto(Resources.RESOURCES)
 
         return "hello";
     }
--- src/main/java/com/project/mgz/models/Book.java
+++
@@ -1,7 +0,0 @@
-package com.project.mgz.models;
-
-
-
-@Entity
-public class Book {
-}
